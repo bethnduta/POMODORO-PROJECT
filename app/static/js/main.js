a15ea155270038876dc8d07a1bbc53bcf558d84b
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-13 22:19:21 +0300

    feat: add pomodoro js timer

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,17 +1,14 @@
+// tapping into DOM elements
 let workTime = document.querySelector("#w_minutes").textContent;
 let breakTime = document.querySelector("#b_minutes").textContent;
 let sessionCount = document.querySelector("#counter").textContent;
 
+// variables for the timmer
 my_session = 5;
 resetBreak = breakTime;
 resetWork = workTime;
 
-// for (let i = 0; i < my_session; i++) {
-//   startBreak();
-// }
-
-startBreak();
-
+// logic for the timmer
 function startBreak() {
   let breakInterval = setInterval(function () {
     if (breakTime > 0) breakTime--;
@@ -42,17 +39,6 @@ function startBreak() {
   }
 }
 
-// function startWork() {
-//   let workCountdown = setInterval(function () {
-//     if (workTime > 0) workTime--;
-//     else {
-//       workTime = 0;
-//       clearInterval(workCountdown);
-//       sessionCount++;
-//       breakTime = reset;
-//       startBreak();
-//     }
-//     document.querySelector("#counter").textContent = sessionCount;
-//     document.querySelector("#w_minutes").textContent = workTime;
-//   }, 1000);
-// }
+if (breakTime != 0 && workTime != 0) {
+  startBreak();
+}
